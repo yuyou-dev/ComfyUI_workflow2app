295a58fd39828dbb27b86f88aab0df1717c9f771
tianxue <747012622@qq.com>
2024-08-28 09:38:42 +0800

    更新sdk api project3_5案例

--- projects/project_3_5/wechat-example/video2video/pages/index/index.js
+++ projects/project_3_5/wechat-example/video2video/pages/index/index.js
@@ -1,105 +1,221 @@
 // index.js
 Page({
   data: {
+    videoUrl: '',
     videoUrlOutput: '',
     genderArray: ['男', '女'],
     genderIndex: 0,
-    baseUrl: "http://192.168.31.101:6010"
+    canUpload: false,
+    baseUrl: "http://192.168.3.189:6020",
+    selected_index: null
   },
-  onGenderChange: function (e) {
+
+  onLoad() {
+    this.initServer();
+  },
+
+  // 初始化服务器
+  initServer() {
+    wx.request({
+      url: `${this.data.baseUrl}/init`,
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        if (res.data.code === 10000) {
+          this.setData({
+            selected_index: res.data.data.selected_index
+          });
+          console.log('Selected index:', res.data.data.selected_index);
+        } else {
+          wx.showToast({
+            title: `Error: ${res.data.message}`,
+            icon: 'none',
+            duration: 2000
+          });
+        }
+      },
+      fail: res => {
+        console.error('Init request failed', res);
+      }
+    });
+  },
+
+  // 性别选择器的变更
+  onGenderChange(e) {
     this.setData({
       genderIndex: e.detail.value
     });
   },
+
   // 选择视频
   chooseMedia() {
     wx.chooseMedia({
       count: 1,
-      mediaType: 'video',
+      mediaType: ['video'],
       success: res => {
-        console.log(res)
-        let videoUrl = res.tempFiles[0].tempFilePath
+        const videoUrl = res.tempFiles[0].tempFilePath;
         this.setData({
           videoUrl: videoUrl
         });
+      },
+      fail: err => {
+        console.error('Video selection failed', err);
       }
     });
   },
+
   // 上传视频
   uploadVideo() {
-    let filePath = this.data.videoUrl
+    const filePath = this.data.videoUrl;
+    if (!filePath) {
+      wx.showToast({
+        title: 'Please select a video first',
+        icon: 'none',
+        duration: 2000
+      });
+      return;
+    }
+
     wx.showLoading({
-      title: '正在生成...',
-    })
+      title: '正在上传...',
+    });
+
     wx.uploadFile({
-      url: this.data.baseUrl + '/upload', // 服务器接收视频的接口
+      url: `${this.data.baseUrl}/upload`,
       filePath: filePath,
-      name: 'file', // 对应服务器端处理文件的字段名
+      name: 'file',
       formData: {
-        'genderIndex': this.data.genderIndex
+        'selected_index': this.data.selected_index
       },
       success: res => {
-        try {
-          const data = JSON.parse(res.data); // 解析JSON字符串
-          console.log('解析后的数据:', data);
-          if (data.prompt_id) {
-            this.getOutput(data.prompt_id);
-          } else {
-            wx.hideLoading()
-            console.error('未收到 prompt_id');
-          }
-        } catch (e) {
-          wx.hideLoading()
-          console.error('解析JSON出错:', e);
+        const data = JSON.parse(res.data);
+        console.log(data)
+        if (data.code === 10000) {
+          this.setData({
+            canUpload: true,
+            file_response_data: data.data
+          })
+          wx.showToast({
+            title: 'Upload successful',
+            icon: 'success',
+            duration: 2000
+          });
+        } else {
+          wx.showToast({
+            title: `Error: ${data.message}`,
+            icon: 'none',
+            duration: 2000
+          });
         }
       },
-      fail: error => {
-        wx.hideLoading()
-        console.error('上传失败', error)
+      fail: err => {
+        console.error('Upload failed', err);
+        wx.showToast({
+          title: 'Upload failed',
+          icon: 'none',
+          duration: 2000
+        });
+      },
+      complete: () => {
+        wx.hideLoading();
       }
-    })
+    });
   },
-  getOutput(prompt_id) {
-    // console.log(prompt_id)
+
+  // 队列提示
+  queuePrompt() {
+    wx.showLoading({
+      title: '正在生成...',
+    });
+
     wx.request({
-      url: this.data.baseUrl + '/get_output',
+      url: `${this.data.baseUrl}/queue`,
       method: 'POST',
       data: {
-        prompt_id: prompt_id
+        gender_index: this.data.genderIndex,
+        selected_index: this.data.selected_index,
+        file_response_data: this.data.file_response_data
       },
       header: {
         'content-type': 'application/json'
       },
       success: res => {
-        console.log(res)
 
-        if (res.data.code == 10000) {
-          this.setData({
-            videoUrlOutput: res.data.video_urls
+        if (res.data.code === 10000) {
+          this.getOutput(res.data.data.prompt_id);
+        } else {
+          wx.hideLoading();
+          wx.showToast({
+            title: `Error: ${res.data.message}`,
+            icon: 'none',
+            duration: 2000
           });
-          wx.hideLoading()
-
-        } else if (res.data.code == 10002) {
-          setTimeout(() => {
-            this.getOutput(prompt_id)
-          }, 2000)
-        }else{
-          wx.hideLoading()
-
         }
-
       },
-      fail: error => {
-        wx.hideLoading()
+      fail: err => {
+        wx.hideLoading();
 
-        console.log(error);
+        console.error('Queue prompt failed', err);
+        wx.showToast({
+          title: 'Queue prompt failed',
+          icon: 'none',
+          duration: 2000
+        });
       }
     });
   },
+
+  // 获取输出
+  getOutput(prompt_id) {
+    wx.request({
+      url: `${this.data.baseUrl}/get_output`,
+      method: 'POST',
+      data: {
+        prompt_id: prompt_id,
+        selected_index: this.data.selected_index
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        if (res.data.code === 10000) {
+          wx.hideLoading();
+
+          const filename = res.data.data.outputs_video.gifs[0].filename;
+          this.setData({
+            videoUrlOutput: `${this.data.baseUrl}/view?filename=${filename}&selected_index=${this.data.selected_index}`
+          });
+        } else if (res.data.code === 10002) {
+          setTimeout(() => {
+            this.getOutput(prompt_id);
+          }, 2000);
+        } else {
+          wx.hideLoading();
+
+          wx.showToast({
+            title: `Error: ${res.data.message}`,
+            icon: 'none',
+            duration: 2000
+          });
+        }
+      },
+      fail: err => {
+        console.error('Get output failed', err);
+        wx.showToast({
+          title: 'Get output failed',
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
+
+  // 重置按钮
   returnBtn() {
     this.setData({
       videoUrlOutput: ''
-    })
-  },
-
-})
+    });
+  }
+});
--- projects/project_3_5/workflow_api.py
+++ projects/project_3_5/workflow_api.py
@@ -1,68 +1,120 @@
-from flask import Flask, send_from_directory,jsonify, request as flask_request
-from flask_cors import CORS
-import sys
-sys.path.append('..')
-from utils.workflow_utils import run_workflow,output_video,update_prompt_workflow
+from flask import Flask, request as flask_request, Response, jsonify
+from comfy_sdk import select_random_server, run_workflow, upload_file, history, show_result
 import json
-from werkzeug.utils import secure_filename
-# 获取视频属性
-# from moviepy.editor import VideoFileClip
-import os
+
 app = Flask(__name__)
-CORS(app)
-api_name = './workflow_api.json'
-# 定义路由，使得外部可以直接访问图片
-@app.route('/video/<filename>')
-def get_image(filename):
-    return send_from_directory('/root/static/video', filename)
-# 上传图片
+
+# 配置目标服务器的URL数组
+target_urls = [
+    'https://xxxxx-xxxx-xxxxxx',
+    None,
+    None,
+    None,
+    None
+]
+
+
+# 统一的响应函数
+def create_response(data=None, code=10000, message="Success"):
+    response = {
+        "code": code,
+        "message": message
+    }
+    if data:
+        response["data"] = data
+    return jsonify(response), 200 if str(code).startswith('1000') else 500
+
+# init 分配到哪台服务器 返回index
+@app.route('/init', methods=['GET'])
+def init():
+    try:
+        
+        target_url, selected_index = select_random_server(target_urls)
+        print(target_url)
+        print(selected_index)
+        return create_response({"selected_index": selected_index})
+    except Exception as e:
+        return create_response(code=40001, message=f"Error: {str(e)}")
+
+# 上传视频
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
-        # 把视频上传到comfyui 
-        if 'file' not in flask_request.files:
-            return '没有文件部分在请求中', 400
-        file = flask_request.files['file']
- 
-        if file:
-            filename = secure_filename(file.filename)
-            file_path = os.path.join('/root/ComfyUI/input/video/', filename)
-            file.save(file_path)
-            # 获取性别
-            genderIndex = flask_request.form.get('genderIndex')
-            # print(genderIndex,'选择性别')
-            # prompt_workflow = json.load(open(api_name))
-            
-            updated_data = update_prompt_workflow(api_name, 'LoadVideoAndSegment_', 'video/'+filename,'video')
-            if int(genderIndex) == 0:
-                updated_data['11']['inputs']['text_positive'] = 'chibi:2, a man with black clothes,jeans,cute,Kawaii,(full body:2),Standing:2,shoe:2,(blue clean background),(Highly saturated background),PVC material, silicone material，standing character, soft smooth lighting, soft pastel colors, skottie young, 3d blender render, polycount, modular constructivism, pop surrealism, physically based rendering, square image'
-            else:
-                updated_data['11']['inputs']['text_positive'] = 'chibi:2, a girl with skirt,cute,Kawaii,(full body:2),Standing:2,shoe:2,(blue clean background),(Highly saturated background),PVC material, silicone material，standing character, soft smooth lighting, soft pastel colors, skottie young, 3d blender render, polycount, modular constructivism, pop surrealism, physically based rendering, square image'
-            
-            if updated_data:
-                prompt_id = run_workflow(updated_data)  # 使用更新后的数据
-                return jsonify({"prompt_id": prompt_id})
-            else:
-                return jsonify({'error': 'Failed to update workflow'}), 500           
-   
-        else:
-            return jsonify({"error": upload_response.status_code}), 500
+        # 获取上传的文件
+        uploaded_file = flask_request.files['file']
+        original_filename = uploaded_file.filename  # 获取原始文件名
+        selected_index = flask_request.form.get('selected_index')
+        # 转发上传请求
+        files = {'image': (original_filename, uploaded_file.stream, uploaded_file.mimetype)}        
+        data = {'subfolder': 'video'}
+        print(target_urls[int(selected_index)],'target_urls[selected_index]')
+        #上传文件并返回参数
+        file_response = upload_file(target_urls[int(selected_index)],files,data)
+        file_response_data = file_response.json()
+        print(file_response_data)
+        # 调用 queue 函数处理后续工作流
+        return create_response(file_response_data)
     except Exception as e:
-        return jsonify({'error': f'Error {str(e)}'}), 500
+        return create_response(code=40002, message=f"Error: {str(e)}")
 
-# 获取output内容
+@app.route('/queue', methods=['POST'])
+def queue():
+    try:
+        data = flask_request.get_json()
+        gender_index = data.get('gender_index')
+        selected_index = data.get('selected_index')
+        file_response_data = data.get('file_response_data')
+        
+        print(data)
+        print(gender_index)
+        print(selected_index)
+        print(file_response_data)
+
+        # 加载并修改工作流参数
+        prompt_workflow = json.load(open('./workflow_api.json'))
+        prompt_workflow['39']['inputs']['video'] = f"{file_response_data['subfolder']}/{file_response_data['name']}"
+
+        if int(gender_index) == 0:
+            prompt_workflow['11']['inputs']['text_positive'] = 'chibi:2, a man with black clothes, jeans, cute, Kawaii, (full body:2), Standing:2, shoe:2, (blue clean background), (Highly saturated background), PVC material, silicone material, standing character, soft smooth lighting, soft pastel colors, skottie young, 3d blender render, polycount, modular constructivism, pop surrealism, physically based rendering, square image'
+        else:
+            prompt_workflow['11']['inputs']['text_positive'] = 'chibi:2, a girl with skirt, cute, Kawaii, (full body:2), Standing:2, shoe:2, (blue clean background), (Highly saturated background), PVC material, silicone material, standing character, soft smooth lighting, soft pastel colors, skottie young, 3d blender render, polycount, modular constructivism, pop surrealism, physically based rendering, square image'
+
+        queue_response = run_workflow(target_urls[int(selected_index)], prompt_workflow)
+        queue_response_data = queue_response.json()
+
+        return create_response(queue_response_data)
+    except Exception as e:
+        return create_response(code=40002, message=f"Error: {str(e)}")
+
+# 轮询history
 @app.route('/get_output', methods=['POST'])
 def get_output():
     try:
         data = flask_request.get_json()
         prompt_id = data.get('prompt_id')
-        response = output_video(prompt_id)
-        return response;
+        selected_index = data.get('selected_index')
+        response_content = history(target_urls[int(selected_index)], prompt_id).json()
+        if not response_content.get(prompt_id, {}):
+            return create_response({"outputs_video": None}, code=10002, message="The value is an empty dictionary.")
+        else:
+            outputs_video = response_content[prompt_id]['outputs']['46']
+            return create_response({"outputs_video": outputs_video})
     except Exception as e:
-        return jsonify({'error': f'Error {str(e)}'}), 500
-        
+        return create_response(code=40002, message=f"Error: {str(e)}")
+
+# 显示
+@app.route('/view')
+def view():
+    try:
+        filename = flask_request.args.get('filename')
+        selected_index = flask_request.args.get('selected_index')
+        if not filename:
+            return create_response(code=40003, message="Filename is required")
+
+        response = show_result(target_urls[int(selected_index)], filename)
+        return Response(response.content, content_type=response.headers['Content-Type'])
+    except Exception as e:
+        return create_response(code=40002, message=f"Error: {str(e)}")
+
 if __name__ == '__main__':
-    app.run(debug=True, port=6010)
-
-
-
+    app.run(port=6020)
