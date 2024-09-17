17b590a802d50b19f367feba592563f9eb04f208
余悠 <34769581+yuyou-dev@users.noreply.github.com>
2024-09-17 23:06:32 +0800

    Update test.py

--- projects/test.py
+++ projects/test.py
@@ -9,29 +9,17 @@ def upload():
     key = flask_request.args.get('key')
     return key
 
+
 @app.route('/queue', methods=['POST'])
 def queue():
-    try:
-        # 加载并修改工作流参数
-        prompt_workflow = json.load(open('./workflow_api.json'))
+    # 加载工作流
+    prompt_workflow = json.load(open('./workflow_api.json'))
 
-        queue_response = run_workflow("server base", prompt_workflow)
-        queue_response_data = queue_response.json()
+    queue_response = run_workflow("server base", prompt_workflow)
+    queue_response_data = queue_response.json()
 
-        return create_response(queue_response_data)
-    except Exception as e:
-        return create_response(code=40002, message=f"Error: {str(e)}")
+    return queue_response_data
 
-
-# 统一的响应函数
-def create_response(data=None, code=10000, message="Success"):
-    response = {
-        "code": code,
-        "message": message
-    }
-    if data:
-        response["data"] = data
-    return jsonify(response), 200 if str(code).startswith('1000') else 500
 
 if __name__ == '__main__':
     app.run(port=6006)
